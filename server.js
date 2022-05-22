14bee19fad1e7e08e67cff3bf721e7b29a11ad2b
ashutoshofficial09 <ashutoshm9695@gmail.com>
2022-05-22 15:24:14 +0530

    database connected

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 const app = require('./app');
-//Server
 
+dotenv.config({ path: './config.env' });
 //console.log(process.env);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>,process.env.DATABASE_PASSWORD'
+// );
+const DB = process.env.DATABASE;
+//console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((con) => console.log('Database connected Successfully'));
+
+const tourSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A toue must have a name'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'the Forest Hik',
+  rating: 4.7,
+  price: 497,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error💥💥💥💥💥', err);
+  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
