621ef54d2c91c3c32ad7aab1476f6b1a1b25fc52
ashutoshoff <carrierliten@gmail.com>
2022-05-27 18:24:37 +0530

    bcrypt | jwt FOR Auth

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minLength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //This works only on SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  //Only run this function if password is modified
+  if (!this.isModified('password')) return next();
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
