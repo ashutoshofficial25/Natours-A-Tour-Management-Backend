758e1d45faff6f8627c9f44c4cdd55729d704f91
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-05-31 12:58:34 +0530

    Populating data | reviewModel created

--- models/tourModel.js
+++ models/tourModel.js
@@ -152,6 +152,14 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
 //AGGREGATION  Middleware
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
