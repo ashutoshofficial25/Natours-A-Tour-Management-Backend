b76f000c1ca4b082a77f12a73d0f71484d9f67b9
ashutoshofficial09 <ashutoshm9695@gmail.com>
2022-06-06 10:27:10 +0530

    app/js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(
 //test middlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  //console.log(req.headers);
   next();
 });
 
